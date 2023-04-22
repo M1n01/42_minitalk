46d57bf5e6de01a6195c47cc0b5b4cb119c159a5
Minato Abe <minato.cat.love@gmail.com>
2023-04-22 19:48:04 +0900

    delete sigaction

--- bonus/server_bonus.c
+++ bonus/server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 13:35:23 by minabe            #+#    #+#             */
-/*   Updated: 2023/04/22 15:51:44 by minabe           ###   ########.fr       */
+/*   Updated: 2023/04/22 19:39:21 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,35 +33,8 @@ static void	init_char(void)
 	return ;
 }
 
-unsigned char	decode_hamming(unsigned short encoded_data)
-{
-	unsigned short	error_bit;
-	unsigned char	decoded;
-
-	error_bit = 0;
-	decoded = 0;
-	error_bit |= ((encoded_data >> 11) ^ (encoded_data >> 9) ^ \
-		(encoded_data >> 7) ^ (encoded_data >> 5) ^ \
-		(encoded_data >> 3) ^ (encoded_data >> 1)) & 1;
-	error_bit |= (((encoded_data >> 10) ^ (encoded_data >> 9) ^ \
-		(encoded_data >> 6) ^ (encoded_data >> 5) ^ (encoded_data >> 2) ^ \
-		(encoded_data >> 1)) & 1) << 1;
-	error_bit |= (((encoded_data >> 8) ^ (encoded_data >> 7) ^ \
-		(encoded_data >> 6) ^ (encoded_data >> 5) ^ (encoded_data)) & 1) << 2;
-	error_bit |= (((encoded_data >> 4) ^ (encoded_data >> 3) ^ \
-		(encoded_data >> 2) ^ (encoded_data >> 1) ^ (encoded_data)) & 1) << 3;
-	if (error_bit)
-		encoded_data ^= 1 << (12 - error_bit);
-	decoded |= (encoded_data & 0b001000000000) >> 2;
-	decoded |= (encoded_data & 0b000011100000) >> 1;
-	decoded |= (encoded_data & 0b000000001111);
-	return (decoded);
-}
-
 static void	receive_bit(int signum, siginfo_t *info, void *context)
 {
-	unsigned short	decoded;
-
 	(void)context;
 	if (signum == SIGUSR1)
 		g_char.parts |= 1 << g_char.current_bit;
@@ -70,15 +43,14 @@ static void	receive_bit(int signum, siginfo_t *info, void *context)
 	g_char.current_bit++;
 	if (g_char.current_bit == 12)
 	{
-		decoded = decode_hamming(g_char.parts);
-		ft_putchar_fd(decoded, 1);
+		ft_putchar_fd(g_char.parts, 1);
 		init_char();
 	}
 	usleep(10);
-	if (info->si_pid != 0)
+	if (info->si_pid > 0)
 		kill(info->si_pid, SIGUSR1);
 	else
-		ft_printf("pid is 0\n");
+		ft_error("pid error\n");
 	return ;
 }
 
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 13:35:23 by minabe            #+#    #+#             */
-/*   Updated: 2023/04/22 15:51:36 by minabe           ###   ########.fr       */
+/*   Updated: 2023/04/22 19:46:07 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,42 +33,14 @@ static void	init_char(void)
 	return ;
 }
 
-unsigned char	decode_hamming(unsigned short encoded_data)
-{
-	unsigned short	error_bit;
-	unsigned char	decoded;
-
-	error_bit = 0;
-	decoded = 0;
-	error_bit |= ((encoded_data >> 11) ^ (encoded_data >> 9) ^ \
-		(encoded_data >> 7) ^ (encoded_data >> 5) ^ \
-		(encoded_data >> 3) ^ (encoded_data >> 1)) & 1;
-	error_bit |= (((encoded_data >> 10) ^ (encoded_data >> 9) ^ \
-		(encoded_data >> 6) ^ (encoded_data >> 5) ^ (encoded_data >> 2) ^ \
-		(encoded_data >> 1)) & 1) << 1;
-	error_bit |= (((encoded_data >> 8) ^ (encoded_data >> 7) ^ \
-		(encoded_data >> 6) ^ (encoded_data >> 5) ^ (encoded_data)) & 1) << 2;
-	error_bit |= (((encoded_data >> 4) ^ (encoded_data >> 3) ^ \
-		(encoded_data >> 2) ^ (encoded_data >> 1) ^ (encoded_data)) & 1) << 3;
-	if (error_bit)
-		encoded_data ^= 1 << (12 - error_bit);
-	decoded |= (encoded_data & 0b001000000000) >> 2;
-	decoded |= (encoded_data & 0b000011100000) >> 1;
-	decoded |= (encoded_data & 0b000000001111);
-	return (decoded);
-}
-
 static void	receive_bit(int signum)
 {
-	unsigned short	decoded;
-
 	if (signum == SIGUSR1)
 		g_char.parts |= 1 << g_char.current_bit;
 	g_char.current_bit++;
 	if (g_char.current_bit == 12)
 	{
-		decoded = decode_hamming(g_char.parts);
-		ft_putchar_fd(decoded, 1);
+		ft_putchar_fd(g_char.parts, 1);
 		init_char();
 	}
 	return ;
@@ -76,16 +48,10 @@ static void	receive_bit(int signum)
 
 static void	receive_msg(void)
 {
-	struct sigaction	s_sa;
-
 	init_char();
-	ft_bzero(&s_sa, sizeof(struct sigaction));
-	s_sa.sa_handler = receive_bit;
-	sigaddset(&s_sa.sa_mask, SIGUSR1);
-	sigaddset(&s_sa.sa_mask, SIGUSR2);
-	if (sigaction(SIGUSR1, &s_sa, NULL) == -1 \
-		|| sigaction(SIGUSR2, &s_sa, NULL) == -1)
-		ft_error("sigaction error\n");
+	if (signal(SIGUSR1, receive_bit) == SIG_ERR \
+		|| signal(SIGUSR2, receive_bit) == SIG_ERR)
+		ft_error("signal error\n");
 	while (1)
 		pause();
 }
